b5d3ee4a8c918f51a7b91d170c351e9f7100c7e0
Aurelie RAOUL <aurelie.raoul@yahoo.fr>
2022-03-04 15:10:56 +0100

    Ajout d'attributs pour les articles

--- bean.py
+++ bean.py
@@ -6,13 +6,16 @@ class Article():
     """_summary_
     """
 
-    def __init__(self, titre, date_parution, auteur, texte, categorie=None, journal=None):
+    def __init__(self, titre, date_parution, url, auteur, texte, resume=None, categorie=None, journal=None, city=None):
         self.titre = titre
         self.date_parution = date_parution
         self.auteur = auteur
         self.texte = texte
         self.categorie = categorie
         self.journal = journal
+        self.url = url
+        self.city = city
+        self.resume = resume
 
     
 class Journal():

b7c6cb7b06ce8eca4b25f4ad0b994c2997a5a1b9
Aurelie RAOUL <aurelie.raoul@yahoo.fr>
2022-03-04 12:18:30 +0100

    Ajout verbose, nettoyage imports, doc

--- scrapping_util.py
+++ scrapping_util.py
@@ -1,15 +1,19 @@
 from urllib import request
 import bs4
-import collections
-import numpy as np
-import pandas as pd
-from datetime import datetime
-from os import getcwd
-from PIL import Image
-import re
 
 
-def get_page(url):
+def get_page(url, verbose=0):
+    """Retrieve the HTML page
+
+    Args:
+        url (str): the page URL
+        verbose (int, optional): Log level. Defaults to 0.
+
+    Returns:
+        BeautifulSoup: the page
+    """
+    if verbose>1:
+        print("Request url :", url)
     req = request.Request(url, headers = {'User-Agent' : 'Mozilla/5.0'})
     html = request.urlopen(req).read()
     return bs4.BeautifulSoup(html, "lxml")

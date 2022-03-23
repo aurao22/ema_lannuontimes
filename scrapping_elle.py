1c53c03ecffd4e001d0fca4f6d1246a6da15ca69
Erwan <erwan.drouillard@gmail.com>
2022-03-23 10:42:18 +0100

    scrapping_elle

---
+++ scrapping_elle.py
@@ -0,0 +1,7 @@
+# Importation des bibliothèques nécessaire
+
+from scrapping_util import get_page, get_selenium_firefox_driver, save_article_in_bdd
+from selenium.webdriver.common.by import By
+import time
+import math
+
